e5652210e2537bd84529a0bbf32b5154db362594
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-29 16:31:56 +0200

    [UPD] creata rotta index

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,9 +1,18 @@
-const { json } = require('express');
 const connection = require('../data/db');
 
+// INDEX - elenco di tutti i film
 const index = (req, res) => {
-    console.log('elenco film')
-}
+    connection.query("SELECT * FROM movies", (err, moviesResult) => {
+        if (err) return res.status(500).json({ error: "Database query failed: " + err });
+
+        const movies = moviesResult.map((movie) => ({
+            ...movie,
+            image: req.imagePath + movie.image
+        }));
+
+        res.json(movies);
+    });
+};
 
 const show = (req, res) => {
     console.log('film con id' + req.params.id)
