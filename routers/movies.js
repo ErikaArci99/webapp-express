a8008632e9d42b6c26beb4542d39bc830a891f71
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-29 16:06:14 +0200

    [FIX] non so cosa sia successo ma ho sistemato

--- routers/movies.js
+++ routers/movies.js
@@ -3,11 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 //importiamo il controller
-<<<<<<< HEAD
 const moviesControllers = require('../controllers/movieControllers');
-=======
-const movieControllers = require('../controllers/movieControllers');
->>>>>>> a892faf49ca459a0bdf6c823d01162795500fad4
 
 // INDEX 
 router.get('/', moviesControllers.index);
