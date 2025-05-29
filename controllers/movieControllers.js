954107da8b9d86984dd1eb176b03e0a12f0b3616
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-29 16:32:39 +0200

    [UPD] creata rotta show

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -14,8 +14,30 @@ const index = (req, res) => {
     });
 };
 
+// SHOW - dettaglio film + recensioni
 const show = (req, res) => {
-    console.log('film con id' + req.params.id)
-}
+    const { id } = req.params;
+
+    const movieSql = "SELECT * FROM movies WHERE id = ?";
+    const reviewsSql = "SELECT * FROM reviews WHERE movie_id = ?";
+
+    connection.query(movieSql, [id], (err, movieResult) => {
+        if (err) return res.status(500).json({ error: "Database query failed: " + err });
+
+        if (movieResult.length === 0 || movieResult[0].id === null) {
+            return res.status(404).json({ error: "Film non trovato" });
+        }
+
+        const movie = movieResult[0];
+
+        connection.query(reviewsSql, [id], (err, reviewResult) => {
+            if (err) return res.status(500).json({ error: "Database query failed: " + err });
+
+            movie.reviews = reviewResult;
+
+            res.json({ ...movie, image: req.imagePath + movie.image });
+        });
+    });
+};
 
 module.exports = { index, show }
