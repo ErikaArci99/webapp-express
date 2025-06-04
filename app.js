fe249ba9566060fc900a294b7195a664223fe78a
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-05 00:15:17 +0200

    [UPD] installato multer per upload immagini

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// mostra un messaggio in console
-console.log('hello world');
-
 // importo express e lo inizializzo
 const express = require('express');
 const app = express();
@@ -8,28 +5,27 @@ const app = express();
 // importo cors
 const cors = require('cors');
 
-// importo dotenv per leggere .env 
-require('dotenv').config();
-
-// uso cors prima degli asset statici
-app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"]
-}));
-
-
-// definisco la porta
-const port = process.env.SERVER_PORT || 3000;
-
 // importo i middleware
 const imagePath = require('./middlewares/imagePath');
 const notFound = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
 
+// uso cors prima degli asset statici
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
+// importo dotenv per leggere .env 
+require('dotenv').config();
+
+// definisco la porta
+const port = process.env.SERVER_PORT || 3000;
+
 // uso il middleware per gli asset statici generici (es. css, js)
 app.use(express.static('public'));
 
 // // uso il middleware per servire immagini da /public/img
-// app.use('/img', express.static('public/img'));
+app.use('/img', express.static('public/img'));
 
 // uso il middleware per parsare il body json
 app.use(express.json());
